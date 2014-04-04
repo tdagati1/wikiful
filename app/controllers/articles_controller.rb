9f6feec65ca7964f9f12aab302b982a0e05b97f5
Anthony Dagati <tdagati.private@gmail.com>
2014-04-04 08:11:00 -0700

    Updated routes and ArticlesController

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.order(updated_at: :desc).limit(25)
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+end
